b5b3d58372c52f5b970548de0ff044f4877ee7d4
Artfulrelic8 <Timothy_21@hotmail.com>
2021-07-27 18:33:32 +0200

    Added benis and FPS counter

---
+++ Tutel-Game/Assets/Scripts/ResourceCount.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class ResourceCount : MonoBehaviour
+{
+
+    public Text text;
+
+    int count = 0;
+    int delay = 1;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= delay )
+        {
+            timer = 0f;
+            if (count < 1000)
+            {
+                count++;
+                text.text = "Water " + count;
+            }
+        }
+    }
+
+}
