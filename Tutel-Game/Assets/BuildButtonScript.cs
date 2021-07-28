2653e3ea71e68494c65c7ca647b3ab8561504639
Zegon <sebeheeb@gmail.com>
2021-07-28 14:30:34 +0200

    build menu
    b

---
+++ Tutel-Game/Assets/BuildButtonScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public GameObject BuildMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
