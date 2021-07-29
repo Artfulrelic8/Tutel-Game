6b988bf00e951c1b699d7c5f7dfc0c76b97806c6
Artfulrelic8 <Timothy_21@hotmail.com>
2021-07-29 13:39:06 +0200

    Fixed Pathfinding

--- Tutel-Game/Assets/Scripts/Navigation.cs
+++ Tutel-Game/Assets/Scripts/Navigation.cs
@@ -8,8 +8,6 @@ public class Navigation : MonoBehaviour
 
     public Transform target;
     NavMeshAgent agent;
-    bool done = false;
-    Vector3 desPos;
 
     // Start is called before the first frame update
     void Start()
@@ -20,22 +18,6 @@ public class Navigation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if ((target.position.x -2 < agent.transform.position.x) | (agent.transform.position.x < target.position.x +2) & !(target.position.z -2 < agent.transform.position.z) | !(agent.transform.position.z < target.position.z +2) & !done)
-        if((Mathf.Abs(Mathf.Abs(target.position.x) - Mathf.Abs(agent.transform.position.x)) <2) & (Mathf.Abs(Mathf.Abs(target.position.z) - Mathf.Abs(agent.transform.position.z)) <2))
-        {
-            //done = false;
-            agent.SetDestination(agent.transform.position);
-           // if (desPos != target.position)
-            //{
-               // done = false;
-           // }
-            
-        } else
-        {
-            //Debug.Log(target.position);
             agent.SetDestination(target.position);
-            //desPos = target.position;
-        }
-        
     }
 }
