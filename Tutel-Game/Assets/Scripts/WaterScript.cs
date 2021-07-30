871e9d7acf2705bbc81e06cb5a6fab3b75547120
Zegon <sebeheeb@gmail.com>
2021-07-30 11:55:06 +0200

    trying to good villager

---
+++ Tutel-Game/Assets/Scripts/WaterScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject[] assignedV = new GameObject[5];
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
