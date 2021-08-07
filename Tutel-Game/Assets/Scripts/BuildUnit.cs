cec02b8dd241663c433b6ed0802bde2524cdadf6
Zegon <sebeheeb@gmail.com>
2021-08-07 15:21:52 +0200

    terrain (gay)

--- Tutel-Game/Assets/Scripts/BuildUnit.cs
+++ Tutel-Game/Assets/Scripts/BuildUnit.cs
@@ -20,20 +20,11 @@ public class BuildUnit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            var rayL = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hitL;
-            if (Physics.Raycast(rayL, out hitL, 1000))
-            {
-                if (hitL.collider.tag == "Town Center")
-                {
+        
                   GameObject GO = Instantiate(Villager, TCSpawn.transform.position, Quaternion.identity);
                     GO.GetComponent<NavDestination>().cam = cam;
                     GO.GetComponent<NavDestination>().ground = ground;
                     GO.GetComponent<NavDestination>().Destination = Destination;
-                }
-            }
-        }
+                
     }
 }
